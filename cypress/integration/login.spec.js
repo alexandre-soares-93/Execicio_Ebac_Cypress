80c1fadc618aa79dd49966b2159428dc2c27a90a
Alexandre Oliveira <asoaresdeoliveira977@gmail.com>
2021-08-21 20:51:04 -0300

    Criação dos testes - Exercicio Modulo 11

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+var faker = require('faker');
+
+context('Funcionalidade Login', () =>{
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    })
+
+    it('Deve fazer login com sucesso', () =>{
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () =>{
+        let usuarioInvalido = faker.internet.email()
+        
+        cy.get('#username').type(usuarioInvalido) // Alteração realizada para não usar a mesma massa de teste 
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir senha invalida', () =>{
+        let senhaInvalida = faker.internet.password()
+        
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type(senhaInvalida)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+})
